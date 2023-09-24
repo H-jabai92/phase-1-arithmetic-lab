7c33a778aca6f31e8d165ca96df7345dde5c0a3f
Hasan <h.mjabai92@gmail.com>
2023-09-24 19:47:58 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+multiply = num1 * num2;
+
+
+
+
+(Math.ceil(Math.random()*10))
+
+
+
+
+
+let num3 = 24;
+let num4 = 5;
+console.log(num3%num4)
+
+
+
+
+let max = Math.max(10, 20, 5, 9, 8, 4)
+console.log(max)
+
